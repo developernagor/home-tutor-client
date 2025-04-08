28e1eb6c2537272438ea42193ed7c795fe5f7502
developernagor <developernagor@gmail.com>
2025-04-08 23:11:18 +0600

    Added Tutor Answers section

--- src/components/QuestionsPage/TutorResponsesQuestions.jsx
+++ src/components/QuestionsPage/TutorResponsesQuestions.jsx
@@ -1,18 +1,44 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import React from 'react'
 
 function TutorResponsesQuestions() {
+
+  const {data: answer = [], isLoading, isError, error} = useQuery({
+    queryKey: ["answer"],
+    queryFn: async() => {
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/solutions`)
+      console.log(res.data)
+      return res.data;
+    }
+  })
+
+  if(isLoading){
+    return <p>Answer is Loading........</p>
+  }
+
+  if(isError){
+    return <p>Error: {error.message}</p>
+  }
+
+
+
   return (
     <section className="py-16 px-4">
   <h2 className="text-3xl font-bold text-center mb-8">Tutor Answers</h2>
   <div className="max-w-5xl mx-auto space-y-6">
-    {[
-      { tutor: "Dr. Smith", answer: "Newton’s First Law states that an object will remain at rest or in motion unless acted upon by an external force.", rating: "⭐ 4.9" },
-      { tutor: "Prof. Lisa", answer: "The quadratic formula is a universal method to solve quadratic equations.", rating: "⭐ 5.0" },
-    ].map((response, index) => (
-      <div key={index} className="border p-6 rounded-lg shadow-md">
-        <h3 className="text-lg font-semibold">{response.tutor}</h3>
-        <p className="mt-2 text-gray-600">{response.answer}</p>
-        <p className="text-yellow-500 text-lg mt-2">{response.rating}</p>
+    {answer.map((response, index) => (
+      <div key={index} className="border p-6 rounded-lg shadow-md flex items-center">
+        <div className='w-9/12'>
+        <h3 className="text-lg font-semibold">{response.noteTitle}</h3>
+        <p className="mt-2 font-bold text-blue-600">{response.noteSolution}</p>
+        <p className="text-yellow-500 text-lg mt-2">{response.noteDescription}</p>
+        </div>
+        <div className='w-3/12'>
+          <p className='bg-black mb-2 text-white text-center rounded-2xl p-1 text-xs'>{response.noteSubject}</p>
+          <p className='bg-black mb-2 text-white text-center rounded-2xl p-1 text-xs'>{response.noteChapter}</p>
+          <p className='bg-black mb-2 text-white text-center rounded-2xl p-1 text-xs'>{response.solutionTime}</p>
+        </div>
       </div>
     ))}
   </div>
